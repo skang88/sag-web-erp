0ac3b88b28e0926eb29c8725b5aa33bb51e7b036
Seokgyun Kang <skang88@asu.edu>
2024-12-18 09:18:11 -0500

    Updata KPI Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,20 @@ function Home() {
 }
 
 function App() {
+
+  const openPopup = () => {
+    const popupWidth = window.screen.width; // 팝업 창 너비
+    const popupHeight = window.screen.height; // 팝업 창 높이
+    const left = window.screenX + (window.outerWidth - popupWidth) / 2; // 화면 중앙에 위치
+    const top = window.screenY + (window.outerHeight - popupHeight) / 2;
+
+    window.open(
+      'http://172.16.220.32:3838', // 외부 앱 URL
+      'KPI Dashboard', // 팝업 창 이름
+      `width=${popupWidth},height=${popupHeight},top=${top},left=${left},resizable=yes,scrollbars=yes`
+    );
+  };
+  
   return (
     <Router>
       <div className="App">
@@ -24,6 +38,11 @@ function App() {
               </li>
               <li>
                 <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <button onClick={openPopup} style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer' }}>
+                  KPI
+                </button>
               </li>
               {/* HR 메뉴 */}
               <li className="menu-item">
