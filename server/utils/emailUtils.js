cce962a8fbe6b8561bf34c368a53e7fbbf162829
Seokgyun Kang <skang88@asu.edu>
2025-06-16 09:28:04 -0400

    email verify

--- server/utils/emailUtils.js
+++ server/utils/emailUtils.js
@@ -10,11 +10,17 @@ exports.sendVerificationEmail = async (email, token) => {
   });
 
   const verificationUrl = `http://172.16.220.32:8001/api/auth/verify-email?token=${token}`;
+  
+  // mailOptions 수정
   const mailOptions = {
     from: process.env.GMAIL_USER,
     to: email,
     subject: 'Email Verification',
-    text: `Please verify your email by clicking the following link: ${verificationUrl}`,
+    // text 속성 대신 html 속성 사용
+    html: `
+      <p>Please verify your email by clicking the following link:</p>
+      <a href="${verificationUrl}" target="_blank">Verify Your Email</a>
+    `,
   };
 
   await transporter.sendMail(mailOptions);
