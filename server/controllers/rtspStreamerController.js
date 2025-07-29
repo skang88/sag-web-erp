8287203459b9683ff8e2516ed6b7fc4fcbf872f0
Seokgyun Kang <skang88@asu.edu>
2025-07-29 13:10:54 -0400

    change

--- server/controllers/rtspStreamerController.js
+++ server/controllers/rtspStreamerController.js
@@ -19,14 +19,9 @@ const startRTSPStream = () => {
     // WebSocket 서버가 모든 네트워크 인터페이스에서 수신하도록 명시적으로 설정합니다.
     // 이렇게 하면 'localhost'와 IP 주소 모두로 접속할 수 있습니다.
     wsOptions: {},
-    // FFmpeg 옵션: 직접 테스트하여 성공한 옵션과 안정성을 위한 옵션을 조합합니다.
+    // FFmpeg 옵션: 라이브러리와의 충돌을 피하기 위해, 성공이 확인된 최소한의 옵션만 남깁니다.
     ffmpegOptions: {
-      // 입력 옵션
-      '-rtsp_transport': 'udp', // 네트워크 안정성을 위해 TCP 사용 (컨테이너 환경 필수)
-
-      // 출력 옵션 (JSMpeg 호환성을 위해 명시적으로 지정)
-      '-f': 'mpegts',           // 출력 포맷: MPEG Transport Stream
-      '-codec:v': 'mpeg1video', // 비디오 코덱: mpeg1video
+      '-rtsp_transport': 'tcp', // 네트워크 안정성을 위해 TCP 사용 (컨테이너 환경 필수)
       '-an': '',                // 오디오 비활성화
       '-r': '30',               // 프레임레이트 고정 (테스트 성공 옵션)
       '-b:v': '800k'            // 비트레이트 고정 (테스트 성공 옵션)
