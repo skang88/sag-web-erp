86f479291c323a786f2a984a163caae8659b927a
Seokgyun Kang <seokgyun.kang@seohanga.com>
2025-09-04 13:02:37 -0400

    test

--- server/scripts/test-recognition.js
+++ server/scripts/test-recognition.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 
 // --- 설정값 (여기를 수정해서 테스트하세요) ---
-const PLATE_NUMBER = "TEST-1243"; // 테스트할 차량 번호
+const PLATE_NUMBER = "TEST-1245"; // 테스트할 차량 번호
 // ----------------------------------------
 
 async function sendTestRequest() {
